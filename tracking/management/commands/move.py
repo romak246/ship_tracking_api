4cc9864ec19c7de0268ee839a690025e60c9ed61
roman.konyakhin <romannkonyakhin@gmail.com>
2023-04-14 12:18:46 +0300

    правки движения кораблей

--- tracking/management/commands/move.py
+++ tracking/management/commands/move.py
@@ -1,11 +1,12 @@
-import math
+import concurrent.futures
 import random
+
 from django.core.management.base import BaseCommand
 from geopy import Point, distance
+from geopy.exc import GeocoderUnavailable
 
 from tracking.helpers.get_location_type import get_location_type
 from tracking.models.ship import Ship
-import concurrent.futures
 
 
 class Command(BaseCommand):
@@ -13,22 +14,30 @@ class Command(BaseCommand):
 
     @staticmethod
     def move_ship(ship):
-        current_location = Point(ship.latitude, ship.longitude)
-        bearing = random.uniform(0, 360)
-        new_location = distance.distance(kilometers=20).destination(current_location, bearing)
-        is_land = get_location_type(new_location.latitude, new_location.longitude)
-        while is_land is not None:
+        try:
+            current_location = Point(ship.latitude, ship.longitude)
+            bearing = random.uniform(0, 360)
+            new_location = distance.distance(kilometers=20).destination(current_location, bearing)
             is_land = get_location_type(new_location.latitude, new_location.longitude)
-        ship.latitude = new_location.latitude
-        ship.longitude = new_location.longitude
-        ship.save()
+            while is_land is not None:
+                is_land = get_location_type(new_location.latitude, new_location.longitude)
+            ship.latitude = new_location.latitude
+            ship.longitude = new_location.longitude
+            ship.save()
+        except GeocoderUnavailable:
+            pass
 
     def handle(self, *args, **options):
-        workers = math.ceil(len(Ship.objects.all())/5)
         while True:
-            with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
                 futures = []
                 for ship in Ship.objects.all():
                     futures.append(executor.submit(self.move_ship, ship))
+
+                for future in concurrent.futures.as_completed(futures):
+                    try:
+                        future.result()
+                    except Exception as e:
+                        print(f'Ошибка в потоке: {e}')
 
             self.stdout.write(self.style.SUCCESS('Все корабли передвинулись на 20 км.'))
